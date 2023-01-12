2513c10bb92101027081b3006274af082540b4dd
Alexis Fonte <alexis.e.fonte@gmail.com>
2023-01-12 00:08:35 -0600

    commit tree ebf49cf837cc5669ff7a2713e79f7d02e45c22eb

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
+require 'pry'
 def oxford_comma(array)
-
+    x = array.length
+    if x == 1
+        array.join
+    elsif x == 2
+        array.join(" and ")
+    else 
+        last = array.pop
+        array << "and #{last}"
+        array.join(", ") 
+    end
 end
+
+oxford_comma(["fiddleheads", "okra", "kohlrabi"])
